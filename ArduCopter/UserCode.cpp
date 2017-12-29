c7b019c26bee94839d4b2dbe3bc1bc3d8f6042d4
xiazibin <xiazibin@gmail.com>
2017-12-29 12:18:07 +0800

    ABPoint merge conflict resolved

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -26,32 +26,91 @@ void Copter::userhook_init()
     #endif	
     //	added end 
 
-    #if FXTX_AUTH == 0
-    	printf("License Disabled\n");
-    // 	//	added by ZhangYong 20170705 for item_int
-    // 	gcs_chan[0]..set_mission_item_int(false);
-    // 	gcs_chan[1].set_mission_item_int(false);
-    // #if MAVLINK_COMM_NUM_BUFFERS > 2
-    // 	gcs_chan[2].set_mission_item_int(false);
-    // #endif
-    // 	//	added end
-    
-    #elif FXTX_AUTH == 1
-    
-    printf("License Enabled\n");	
-    // 	//	added by ZhangYong 20170705 for item_int
-    // 	gcs_chan[0].set_mission_item_int(true);
-    // 	gcs_chan[1].set_mission_item_int(true);
-    // #if MAVLINK_COMM_NUM_BUFFERS > 2
-    // 	gcs_chan[2].set_mission_item_int(true);
-    // #endif
-    // 	//	added end
+#if FXTX_AUTH == 0
+	printf("License Disabled\n");
+	//	added by ZhangYong 20170705 for item_int
+	gcs().chan(0).set_mission_item_int(false);
+	gcs().chan(1).set_mission_item_int(false);
+#if MAVLINK_COMM_NUM_BUFFERS > 2
+	gcs().chan(2).set_mission_item_int(false);
+#endif
+	//	added end
+
+#elif FXTX_AUTH == 1
+	printf("License Enabled\n");	
+	
+	//	added by ZhangYong 20170705 for item_int
+	gcs().chan(0).set_mission_item_int(true);
+	gcs().chan(1).set_mission_item_int(true);
+#if MAVLINK_COMM_NUM_BUFFERS > 2
+	gcs().chan(2).set_mission_item_int(true);
+#endif
+	//	added end
+
+#endif
 
     //	added by ZhangYong 20170705
-    printf("Mavlink capabilities %x\n", hal.util->get_capabilities());
+    //printf("Mavlink capabilities %x\n", hal.util->get_capabilities());
     //	added end
+		//	added by ZhangYong 20170712
+#if PJTPASSOSD == ENABLED
+	if(nullptr != (serial_manager.find_serial(AP_SerialManager::SerialProtocol_PassOSD, 0)))
+	{
+		passosd.init(serial_manager);
+	}
+	else
+	{
+		
+	}
+#endif
+	
+#if PROJECTGKXN == ENABLED
+	if(nullptr != (serial_manager.find_serial(AP_SerialManager::SerialProtocol_FlowMeter_GKXN,0)))
+	{
+		flowmeter.init(serial_manager);
+	}
+	//height_replace = 0;
+		
+#endif
+	
+#if BCBMONITOR == ENABLED
+	
+	if(nullptr != (serial_manager.find_serial(AP_SerialManager::SerialProtocol_BCBMonitor,0)))
+	{
+		bcbmonitor.init(serial_manager);
+	}
+		
+#endif
+	
+#if BCBPMBUS == ENABLED
+	printf("PROJECTBCB\n");
+	printf("PMBUS\n");
+		
+	if(nullptr !=  serial_manager.find_serial(AP_SerialManager::SerialProtocol_BCBPMBus,0))
+	{
+		init_bcbpmbus();
+	}
+#endif
+	
+#if PJTPASSOSD == ENABLED
+	printf("PJTPASSOSD\n");
+#endif
+	
+	
+	
+#if PROJECTGKXN == ENABLED
+	printf("PROJECTGKXN\n");
+#endif
+	
+	
+	//	added ebd
+	
+	//	added by ZhangYong 20170705
+	printf("Mavlink capabilities %x\n", hal.util->get_capabilities());
+	//	added end
+
     
-    edit_management.words = g.edition_management;
+    /*edit_management.words = g.edition_management;
     
     printf("major_edition = 0x%x\n", edit_management.data.major_edition);
     printf("project_edition = 0x%x\n", edit_management.data.project_edition);
@@ -63,10 +122,37 @@ void Copter::userhook_init()
     	edit_management.data.major_edition = 2;
     
     	g.edition_management.set_and_save(edit_management.words);
-    }
+    }*/
+	edit_management.words = g.edition_management;
 
+	if(edit_management.data.major_edition != 2)
+	{
+		edit_management.data.major_edition = 2;
+	}
 
-    #endif
+#if PROJECTGKXN == ENABLED
+	if(edit_management.data.project_edition != 1)
+	{
+		edit_management.data.project_edition = 1;
+	}
+#endif	
+
+	//	improve the minor edition from 3 to 4
+	//	failsafe rc gcs
+	//	improve the minor edition from 4 to 5
+	//	ABPoint, dual GPS antenna heaidng
+	if(edit_management.data.minor_edition <= 4)
+	{	
+		edit_management.data.minor_edition = 5;
+	}
+
+	g.edition_management.set_and_save(edit_management.words);
+
+	printf("major_edition = 0x%x\n", edit_management.data.major_edition);
+	printf("project_edition = 0x%x\n", edit_management.data.project_edition);
+	printf("minor_edition = 0x%x\n", edit_management.data.minor_edition);
+	printf("revision_edition = 0x%x\n", edit_management.data.revision_edition);    
+
 
     task.init();
     PtzControl.init();
--- ArduCopter/system.cpp
+++ ArduCopter/system.cpp
@@ -195,6 +195,7 @@ void Copter::init_ardupilot()
 //	added by ZhangYong
 	//	get board id
 #if FXTX_AUTH == 1
+/*
 	memset(auth_msg, 0, 50);
 	memset(auth_id, 0, AUTH_ID_LEN);
 
@@ -208,7 +209,7 @@ void Copter::init_ardupilot()
 		     (unsigned)auth_id[0], (unsigned)auth_id[1], (unsigned)auth_id[2], (unsigned)auth_id[3], (unsigned)auth_id[4], (unsigned)auth_id[5],	\
 		     (unsigned)auth_id[6], (unsigned)auth_id[7], (unsigned)auth_id[8], (unsigned)auth_id[9], (unsigned)auth_id[10], (unsigned)auth_id[11]);
 	
-
+*/
 #endif	
 	
     //	added end 
@@ -293,119 +294,6 @@ void Copter::init_ardupilot()
 
     // disable safety if requested
     BoardConfig.init_safety();
-
-#if FXTX_AUTH == 0
-	printf("License Disabled\n");
-	//	added by ZhangYong 20170705 for item_int
-	gcs().chan(0).set_mission_item_int(false);
-	gcs().chan(1).set_mission_item_int(false);
-#if MAVLINK_COMM_NUM_BUFFERS > 2
-	gcs().chan(2).set_mission_item_int(false);
-#endif
-	//	added end
-
-#elif FXTX_AUTH == 1
-	printf("License Enabled\n");	
-	
-	//	added by ZhangYong 20170705 for item_int
-	gcs().chan(0).set_mission_item_int(true);
-	gcs().chan(1).set_mission_item_int(true);
-#if MAVLINK_COMM_NUM_BUFFERS > 2
-	gcs().chan(2).set_mission_item_int(true);
-#endif
-	//	added end
-
-#endif
-
-	//	added by ZhangYong 20170712
-#if PJTPASSOSD == ENABLED
-	if(nullptr != (serial_manager.find_serial(AP_SerialManager::SerialProtocol_PassOSD, 0)))
-	{
-		passosd.init(serial_manager);
-	}
-	else
-	{
-		
-	}
-#endif
-
-#if PROJECTGKXN == ENABLED
-	if(nullptr != (serial_manager.find_serial(AP_SerialManager::SerialProtocol_FlowMeter_GKXN,0)))
-	{
-		flowmeter.init(serial_manager);
-	}
-
-	//height_replace = 0;
-	
-#endif
-
-#if BCBMONITOR == ENABLED
-
-	if(nullptr != (serial_manager.find_serial(AP_SerialManager::SerialProtocol_BCBMonitor,0)))
-	{
-		bcbmonitor.init(serial_manager);
-	}
-	
-#endif
-
-#if BCBPMBUS == ENABLED
-	printf("PROJECTBCB\n");
-	printf("PMBUS\n");
-	
-	if(nullptr !=  serial_manager.find_serial(AP_SerialManager::SerialProtocol_BCBPMBus,0))
-	{
-		init_bcbpmbus();
-	}
-#endif
-
-#if PJTPASSOSD == ENABLED
-	printf("PJTPASSOSD\n");
-#endif
-
-
-
-#if PROJECTGKXN == ENABLED
-	printf("PROJECTGKXN\n");
-#endif
-
-
-	//	added ebd
-
-	//	added by ZhangYong 20170705
-	printf("Mavlink capabilities %x\n", hal.util->get_capabilities());
-	//	added end
-
-
-
-	edit_management.words = g.edition_management;
-
-	if(edit_management.data.major_edition != 2)
-	{
-		edit_management.data.major_edition = 2;
-	}
-
-#if PROJECTGKXN == ENABLED
-	if(edit_management.data.project_edition != 1)
-	{
-		edit_management.data.project_edition = 1;
-	}
-#endif	
-
-	//	improve the minor edition from 3 to 4
-	//	failsafe rc gcs
-	if(edit_management.data.minor_edition <= 3)
-	{	
-		edit_management.data.minor_edition = 4;
-	}
-
-	g.edition_management.set_and_save(edit_management.words);
-
-	printf("major_edition = 0x%x\n", edit_management.data.major_edition);
-	printf("project_edition = 0x%x\n", edit_management.data.project_edition);
-	printf("minor_edition = 0x%x\n", edit_management.data.minor_edition);
-	printf("revision_edition = 0x%x\n", edit_management.data.revision_edition);
-
-
 
 
 //	duration_cnt = 0;
