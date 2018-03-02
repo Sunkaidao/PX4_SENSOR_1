48d13b6a16e3c444514d701775f4752b21a1e98a
BreederBai <1129699006@qq.com>
2018-03-02 17:29:00 +0800

    新功能：放弃JSON格式，使用自定义数据协议向服务器发送实时状态
    由于使用JSON格式，时间和空间开销都比较大，所以放弃JSON格式。

--- libraries/AP_NewBroadcast/AP_NewBroadcast.h
+++ libraries/AP_NewBroadcast/AP_NewBroadcast.h
@@ -19,16 +19,19 @@
 #include <uavcan/helpers/heap_based_pool_allocator.hpp>
 #include "cJSON.h"
 
-#if NEWBROADCAST == ENABLED
+#if NEWBROADCAST == ENABLED 
 
 using namespace PX4;
+
+#define REG_NO_STRING_LEN 32
+#define PAYLOAD_ARRAY_LEN 98
 
 typedef struct
 {
     int32_t action;
-    char number_buffer[26]; /* temporary buffer to print the number into */
+    char reg_no[REG_NO_STRING_LEN]; /* temporary buffer to print the number into */
     int32_t flight_seq;
-    int32_t now_time;
+    uint64_t now_time;
     int32_t state;
     int32_t flight_time;
     int32_t longitude;
@@ -46,6 +49,41 @@ typedef struct
     int32_t spray_range;
 }Message_info;
 
+#pragma  pack (push,1)
+typedef struct
+{
+	uint8_t frame_header0;
+	uint8_t frame_header1;
+    uint8_t action;
+    char reg_no[REG_NO_STRING_LEN]; /* temporary buffer to print the number into */
+    uint16_t flight_seq;
+    uint64_t now_time;
+    uint8_t state;
+    uint32_t flight_time;
+    int32_t longitude;
+    int32_t latitude;
+    int32_t height;
+    int32_t altitude;
+    int16_t path_angle;
+    int16_t pitch_angle;
+    int16_t roll_angle;
+    int16_t horizontal_velocity;
+    int16_t is_nozzle_work;
+    int16_t nozzle_diameter;
+    int16_t nozzle_angle;
+    int16_t nozzle_pressure;
+    int16_t spray_range;
+	uint8_t reserved[14];
+}Payload_s;
+
+typedef union
+{
+	Payload_s _payload_s;
+	char paylod_array[PAYLOAD_ARRAY_LEN];
+}Message_send_union;
+
+#pragma pack(pop)  
+
 enum frameType
 {
     STANDARDID,
@@ -57,14 +95,6 @@ public:
     AP_NewBroadcast(AC_Sprayer& _sprayer);
     ~AP_NewBroadcast();
 
-    char * makeViewJson();
-    void sendJson(char *pJString);
-    void sendJson(char *pJString,uint16_t len);
-    bool sendString(frameType type,char *pJString,uint16_t index,uint8_t length);
-
-    //separator symbol is \n
-    bool sendSeparatorSymbol(frameType type);
-
 	static const struct AP_Param::GroupInfo var_info[];
 
 private:
@@ -73,11 +103,14 @@ private:
     AC_Sprayer & sprayer;
 
     AP_Int8  _enable;
-    AP_Int32 _reg_no;
+	AP_Int8  _reg_no[REG_NO_STRING_LEN];
     AP_Int32 _flight_seq;
 	uint64_t timer;
 
     Message_info view;
+	Message_send_union payload;
+
+	static const uint16_t crc16tab[];
 public:
 	void update();
 	void init();
@@ -102,6 +135,22 @@ public:
     void update_view_nozzle_angle();
     void update_view_nozzle_pressure();
     void update_view_spray_range();
+	void update_payload();
+
+	//Big-endian to Little-endian
+	uint16_t endian_big_to_little16u(uint16_t date16u);
+	int16_t endian_big_to_little16(int16_t date16);
+	uint32_t endian_big_to_little32u(uint32_t date32u);
+	int32_t endian_big_to_little32(int32_t date32);
+	uint64_t endian_big_to_little64u(uint64_t date64u);
+
+	void sendPayload(char *pArray);
+	void sendPayload(char *pArray,uint16_t len);
+	bool sendDataStream(frameType type,char *pArray,uint16_t index,uint8_t length);
+	bool sendCrc16(frameType type);
+	uint16_t crc16_ccitt(const char *buf, int len);
+
+
 };
 #endif
 
